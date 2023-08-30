6e8fce7613d7b158bde1fec2fca0201f158dcadf
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-08-30 23:29:22 +0500

    another commit

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ let justifyContentState = 1; // Initial state is flex-start
 const circleBg = document.getElementById('circleBg');
 const circle = document.getElementById('circle');
 
-circleBg.addEventListener('click', function() {
-  // Update the justifyContentState based on the clicked position
+circleBg.addEventListener('click', function(event) {
   const clickPosition = event.clientX;
   const circleBgRect = circleBg.getBoundingClientRect();
   const circleCenter = circleBgRect.left + circleBgRect.width / 2;
-  
+
   if (clickPosition < circleCenter) {
     justifyContentState = 1;
   } else if (clickPosition > circleCenter + circleBgRect.width / 6) {
@@ -20,56 +19,55 @@ circleBg.addEventListener('click', function() {
   } else {
     justifyContentState = 2;
   }
-  
+
   updateCirclePosition();
+  updateColorScheme();
 });
 
-function updateCirclePosition() {
-    switch (justifyContentState) {
-      case 1:
-        circleBg.style.justifyContent = 'flex-start';
-        circle.classList.remove('theme2', 'theme3');
-        circle.classList.add('theme1');
-        break;
-      case 2:
-        circleBg.style.justifyContent = 'center';
-        circle.classList.remove('theme1', 'theme3');
-        circle.classList.add('theme2');
-        break;
-      case 3:
-        circleBg.style.justifyContent = 'flex-end';
-        circle.classList.remove('theme1', 'theme2');
-        circle.classList.add('theme3');
-        break;
-    }
-  }
-  
-// Update circle position and color scheme function
 function updateColorScheme() {
-    let theme;
-  
-    switch (justifyContentState) {
-      case 1:
-        theme = 'theme1';
-        break;
-      case 2:
-        theme = 'theme2';
-        break;
-      case 3:
-        theme = 'theme3';
-        break;
-      default:
-        theme = 'theme1';
-        break;
-    }
-  
-    document.documentElement.style.setProperty('--theme', theme);
-  }
-  
-  // ... Rest of the JavaScript code ...
-  
+  let theme;
 
-updateCirclePosition(); // Set initial circle position
+  switch (justifyContentState) {
+    case 1:
+      theme = 'theme1';
+      break;
+    case 2:
+      theme = 'theme2';
+      break;
+    case 3:
+      theme = 'theme3';
+      break;
+    default:
+      theme = 'theme1';
+      break;
+  }
+
+  document.documentElement.style.setProperty('--background-color-theme', `var(--${theme})`);
+  // Update other color properties as well using the respective custom property names
+}
+
+function updateCirclePosition() {
+  switch (justifyContentState) {
+    case 1:
+      circleBg.style.justifyContent = 'flex-start';
+      circle.classList.remove('theme2', 'theme3');
+      circle.classList.add('theme1');
+      break;
+    case 2:
+      circleBg.style.justifyContent = 'center';
+      circle.classList.remove('theme1', 'theme3');
+      circle.classList.add('theme2');
+      break;
+    case 3:
+      circleBg.style.justifyContent = 'flex-end';
+      circle.classList.remove('theme1', 'theme2');
+      circle.classList.add('theme3');
+      break;
+  }
+}
+
+// Rest of your JavaScript code ...
+
 
 
 
