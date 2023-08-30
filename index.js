8ffb31dae3173298751ef17861a0a58ec24ba263
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-08-31 00:18:51 +0500

    another one!

--- index.js
+++ index.js
@@ -2,75 +2,18 @@ let finaloutput = [];
 let screen_el = document.querySelector(".output");
 let updateScreen_el = document.querySelector(".equal");
 
-let justifyContentState = 1; // Initial state is flex-start
+const circleBg = document.getElementById("circleBg");
+const circle = document.getElementById("circle");
 
-const circleBg = document.getElementById('circleBg');
-const circle = document.getElementById('circle');
+// Add an event listener to each theme circle
+document.querySelectorAll(".circle").forEach((themeCircle, index) => {
+    themeCircle.addEventListener("click", () => {
+        document.body.dataset.theme = index + 1; // Set the data-theme attribute
+        circle.style.transform = `translateX(calc(${index + 1} * 50%))`; // Move the circle
 
-circleBg.addEventListener('click', function(event) {
-  const clickPosition = event.clientX;
-  const circleBgRect = circleBg.getBoundingClientRect();
-  const circleCenter = circleBgRect.left + circleBgRect.width / 2;
-
-  if (clickPosition < circleCenter) {
-    justifyContentState = 1;
-  } else if (clickPosition > circleCenter + circleBgRect.width / 6) {
-    justifyContentState = 3;
-  } else {
-    justifyContentState = 2;
-  }
-
-  updateCirclePosition();
-  updateColorScheme();
+        // You can add more logic to update other CSS variables based on the selected theme
+    });
 });
-
-function updateColorScheme() {
-  let theme;
-
-  switch (justifyContentState) {
-    case 1:
-      theme = 'theme1';
-      break;
-    case 2:
-      theme = 'theme2';
-      break;
-    case 3:
-      theme = 'theme3';
-      break;
-    default:
-      theme = 'theme1';
-      break;
-  }
-
-  document.documentElement.style.setProperty('--background-color-theme', `var(--${theme})`);
-  // Update other color properties as well using the respective custom property names
-}
-
-function updateCirclePosition() {
-  switch (justifyContentState) {
-    case 1:
-      circleBg.style.justifyContent = 'flex-start';
-      circle.classList.remove('theme2', 'theme3');
-      circle.classList.add('theme1');
-      break;
-    case 2:
-      circleBg.style.justifyContent = 'center';
-      circle.classList.remove('theme1', 'theme3');
-      circle.classList.add('theme2');
-      break;
-    case 3:
-      circleBg.style.justifyContent = 'flex-end';
-      circle.classList.remove('theme1', 'theme2');
-      circle.classList.add('theme3');
-      break;
-  }
-}
-
-// Rest of your JavaScript code ...
-
-
-
-
 
 function del() {
     if (finaloutput.length > 0) {
