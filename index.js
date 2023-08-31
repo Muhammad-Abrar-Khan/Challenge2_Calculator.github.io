1351f7d5e8cf9881b04fcc1717cca8a21181152b
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-09-01 00:41:54 +0500

    another commit

--- index.js
+++ index.js
@@ -2,53 +2,95 @@ let finaloutput = [];
 let screen_el = document.querySelector(".output");
 let updateScreen_el = document.querySelector(".equal");
 
-const circleBg = document.getElementById("circleBg");
-const circle = document.getElementById("circle");
+let justifyContentState = 1; 
 
-// Add an event listener to each theme circle
-document.querySelectorAll(".circle").forEach((themeCircle, index) => {
-    themeCircle.addEventListener("click", () => {
-        document.body.dataset.theme = index + 1; // Set the data-theme attribute
-        circle.style.transform = `translateX(calc(${index + 1} * 50%))`; // Move the circle
+const circleBg = document.getElementById('circleBg');
+const circle = document.getElementById('circle');
+const body = document.querySelector('body');
 
-        // You can add more logic to update other CSS variables based on the selected theme
-    });
+updateCirclePosition();
+updateTheme();
+
+circleBg.addEventListener('click', function(event) {
+  const clickPosition = event.clientX;
+  const circleBgRect = circleBg.getBoundingClientRect();
+  const circleCenter = circleBgRect.left + circleBgRect.width / 2;
+
+  if (clickPosition < circleCenter) {
+    justifyContentState = 1;
+  } else if (clickPosition > circleCenter + circleBgRect.width / 6) {
+    justifyContentState = 3;
+  } else {
+    justifyContentState = 2;
+  }
+
+  updateCirclePosition();
+  updateTheme();
 });
 
+function updateCirclePosition() {
+  switch (justifyContentState) {
+    case 1:
+      circleBg.style.justifyContent = 'flex-start';
+      break;
+    case 2:
+      circleBg.style.justifyContent = 'center';
+      break;
+    case 3:
+      circleBg.style.justifyContent = 'flex-end';
+      break;
+  }
+}
+
+function updateTheme() {
+  body.className = ''; // Clear all classes
+  switch (justifyContentState) {
+    case 1:
+      body.classList.add('theme1');
+      break;
+    case 2:
+      body.classList.add('theme2');
+      break;
+    case 3:
+      body.classList.add('theme3');
+      break;
+  }
+}
+
 function del() {
-    if (finaloutput.length > 0) {
-        finaloutput.pop();
-        updateScreen();
-    }
+  if (finaloutput.length > 0) {
+    finaloutput.pop();
+    updateScreen();
+  }
 }
 
 function updateScreen() {
-    screen_el.textContent = finaloutput.join('');
-    setTimeout(function() {
-        screen_el.scrollLeft = screen_el.scrollWidth;
-    }, 10);
+  screen_el.textContent = finaloutput.join('');
+  setTimeout(function() {
+    screen_el.scrollLeft = screen_el.scrollWidth;
+  }, 10);
 }
 
 function cancel() {
-    finaloutput = [];
-    screen_el.textContent = 0;
+  finaloutput = [];
+  screen_el.textContent = 0;
 }
 
 function addValue(value) {
-    finaloutput.push(value);
-    updateScreen();
+  finaloutput.push(value);
+  updateScreen();
 }
 
 updateScreen_el.addEventListener("click", function() {
-    const expression = finaloutput.join('');
-    let result;
-    try {
-        result = eval(expression);
-        screen_el.textContent = result;
-        finaloutput = [result.toString()]; 
-    } catch (error) {
-        screen_el.textContent = 'Error';
-    }
+  const expression = finaloutput.join('');
+  let result;
+  try {
+    result = eval(expression);
+    screen_el.textContent = result;
+    finaloutput = [result.toString()];
+  } catch (error) {
+    screen_el.textContent = 'Error';
+  }
 });
 
 function zero() { addValue("0"); }
